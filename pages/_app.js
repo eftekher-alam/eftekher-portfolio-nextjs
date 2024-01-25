fa9a38e5a56f917e8f862caf60474aac154435db
eftekher-alam <eftekher@yahoo.com>
2024-01-25 15:03:23 +0600

    Transition animation is done.

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,24 @@ import '../styles/globals.css';
 
 //components import 
 import Layout from '../components/Layout';
+import Transition from "../components/Transition"
+
+//router
+import { useRouter } from "next/router";
+
+//framer motion
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition></Transition>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   )
 }
