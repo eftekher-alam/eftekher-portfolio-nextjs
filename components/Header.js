b8680d762fe504ce2144abca5ea108bfbab0d133
eftekher-alam <eftekher@yahoo.com>
2024-03-23 21:06:55 +0600

    header correction and about page done for all decive

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 import Socials from "../components/Socials";
 
 const Header = () => {
-  return <header className="absolute z-30 w-full flex items-center px-10 xl:px-0 xl:h-[90px] ">
+  return <header className="absolute z-30 w-full flex items-center px-10 xl:px-0 xl:h-[80px]">
     <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-y-4 py-5 md:py-8">
         {/* logo */}
         <Link href={"/"}>
           <h2 className="text-white text-2xl font-bold">eftekher <span className="font-extralight">alam</span><span className="text-[#FF2B2E]">.</span></h2>
--- pages/about/index.js
+++ pages/about/index.js
@@ -8,16 +8,25 @@ import {
   FaCss3,
   FaJs,
   FaReact,
-  FaWordpress,
+  FaBootstrap,
   FaFigma,
+  FaNodeJs,
+  FaGit,
+  FaGithub
 } from "react-icons/fa";
 
 import {
   SiNextdotjs,
-  SiFramer,
+  SiTailwindcss,
   SiAdobexd,
   SiAdobephotoshop,
+  SiExpress,
+  SiMongodb,
+  SiFirebase,
+  SiMicrosoftsqlserver
 } from "react-icons/si";
+
+import { TbBrandMysql, } from "react-icons/tb";
 
 
 //  data
@@ -26,33 +35,37 @@ export const aboutData = [
     title: 'skills',
     info: [
       {
-        title: 'Web Development',
+        title: 'Front End Development',
         icons: [
           <FaHtml5 key={1} />,
           <FaCss3 key={2} />,
           <FaJs key={3} />,
           <FaReact key={4} />,
-          <SiNextdotjs key={5} />,
-          <SiFramer key={6} />,
-          <FaWordpress key={7} />,
+          // <SiNextdotjs key={5} />,
+          <SiTailwindcss key={6} />,
+          <FaBootstrap key={7} />,
         ],
       },
       {
-        title: 'UI/UX Design',
-        icons: [<FaFigma key={1} />, <SiAdobexd key={2} />, <SiAdobephotoshop key={3} />],
+        title: 'Back End Development',
+        icons: [
+          <FaNodeJs key={1} />,
+          // <SiExpress  key={2} />,
+          <p className="text-[22px] pb-1 leading-5" key={2}>ex</p>,
+          <p className="text-base" key={3}>.NET</p>,
+          <SiMongodb key={5} />,
+          <SiMicrosoftsqlserver key={6} />,
+          <TbBrandMysql key={4} />,
+        ],
       },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        title: 'Others',
+        icons: [
+          <FaGit key={1} />,
+          <FaGithub key={2} />,
+          <SiFirebase key={3} />,
+          <FaFigma key={1} />,
+        ],
       },
     ],
   },
@@ -60,33 +73,41 @@ export const aboutData = [
     title: 'experience',
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: 'Full Stack Developer',
+        institute: "Pinovation Tech Ltd, Dhaka, BD",
+        stage: 'Jan 2023 - Dec 2023 (1 Year)',
+      }
+    ],
+  },
+  {
+    title: 'Training',
+    info: [
+      {
+        title: 'Complete Web Development Course With Jhankar Mahbub',
+        institute: "Programming Hero",
+        stage: 'Jul 2023 – Dec 2023 (6 month)',
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
+        title: 'Web Application Development using ASP.NET',
+        institute: "sDB-BISEW",
+        stage: 'Mar 2021 – Sep 2021 (460 hour)',
       },
     ],
   },
   {
-    title: 'credentials',
+    title: 'education',
     info: [
       {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        title: 'BSc in Computer Science and Engineering',
+        institute: "Bangladesh University of Business and Technology, Dhaka, BD",
+        cgpa: "3.91 out of 4.00",
+        stage: 'Dec 2020 - May 2024',
       },
       {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: 'Diploma in Data Telecommunication and Networking',
+        institute: "Feni Computer Institute, Feni, BD",
+        cgpa: "3.20 out of 4.00",
+        stage: 'May 2016 - May 2019',
       },
     ],
   },
@@ -112,29 +133,40 @@ const About = () => {
     >
       <Avatar />
     </motion.div>
-    <div className="container mx-auto h-full flex flex-col items-center lg:flex-row gap-x-6">
+    <div className="container mx-auto h-full flex flex-col items-center lg:flex-row gap-x-6 lg:pt-[1%] ">
       {/* text */}
       <div className="flex-1 flex flex-col justify-center">
-        <motion.h2 
+        <motion.h2
           variants={fadeIn("right", 0.2)}
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="h2"
+          className="h2 max-md:text-2xl max-xl:text-4xl"
         >
           Captivating <span className="text-accent">stories</span> birth magnificent designs.
         </motion.h2>
+        {/* for large screen */}
         <motion.p
           variants={fadeIn("right", 0.4)}
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="max-w-[500px] mx-auto lg:mx-0 mb-6 lg:mb-12 px-2 lg:px-0"
-         >
-          {`10 years ago, I began freelancing as developer. Since then, I've donne remote work for agencies, counsulted fro startups, and collaborated on digital products for business and consumer use.`}
+          className="max-w-[500px] mx-auto lg:mx-0 mb-6 lg:mb-12 px-2 lg:px-0 max-md:hidden"
+        >
+          {`In 2016, I began learning programming. Since then, I've learned multiple programming languages, data structures, and algorithms. Furthermore, I've worked for a software company for almost 1 year.`}
+        </motion.p>
+        {/* from small screen */}
+        <motion.p
+          variants={fadeIn("right", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="max-w-[500px] mx-auto lg:mx-0 mb-4 px-2 md:hidden"
+        >
+          {`In 2016, I began learning programming. Since then, I've learned multiple languages, data structures, and algorithms.`}
         </motion.p>
         {/* counters */}
-        <motion.div 
+        <motion.div
           variants={fadeIn("right", 0.6)}
           initial="hidden"
           animate="show"
@@ -143,25 +175,25 @@ const About = () => {
         >
           <div className="flex flex-1 lg:gap-x-6">
             {/* experience */}
-            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0" >
-              <div className="text-2xl lg:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={10} duration={10} /> +
+            <div className="relative flex-1 lg:after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:left-0" >
+              <div className="text-2xl lg:text-4xl font-extrabold text-accent mb-2 ml-5">
+                <CountUp start={0} end={12} duration={5} /> +
               </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Years of experience</div>
-            </div>
-            {/* Clients */}
-            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0" >
-              <div className="text-2xl lg:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={250} duration={5} /> +
-              </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Satisfied clients</div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] ml-5">Months of experience</div>
             </div>
             {/* Projects */}
-            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0" >
-              <div className="text-2xl lg:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={250} duration={5} /> +
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:left-0" >
+              <div className="text-2xl lg:text-4xl font-extrabold text-accent mb-2 ml-5">
+                <CountUp start={0} end={20} duration={5} /> +
               </div>
-              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Finished projects</div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] ml-5">Finished projects</div>
+            </div>
+            {/* Professional Training Counter */}
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:left-0" >
+              <div className="text-2xl lg:text-4xl font-extrabold text-accent mb-2 ml-5">
+                <CountUp start={0} end={2} duration={8} /> +
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[150px] ml-5">Professional training</div>
             </div>
           </div>
         </motion.div>
@@ -172,8 +204,9 @@ const About = () => {
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="flex flex-col w-full lg:max-w-[48%] h-[480px]"
-       >
+        className="flex flex-col w-full lg:max-w-[48%] h-[480px]  lg:pt-[1%]"
+      >
+        {/* title */}
         <div className="flex gap-x-4 lg:gap-x-8 mx-auto xl:mx-0 mb-4" >
           {aboutData.map((item, itemIndex) => {
             return (
@@ -191,14 +224,15 @@ const About = () => {
         <div className="py-2 lg:py-6 flex flex-col gap-y-2 lg:gap-y-4 items-center lg:items-start">
           {aboutData[index].info.map((item, itemIndex) => {
             return (
-              <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/70">
+              <div key={itemIndex} className="flex-1 flex flex-col max-w-max text-white/70">
                 {/* title */}
-                <div className="font-light mb-2 md:mb-0">{item.title}</div>
-                <div className="hidden md:flex">-</div>
+                <div className="font-semibold text-white">{item.title}</div>
+                <div>{item?.institute}</div>
+                <div>{item?.cgpa ? "CGPA : " + item?.cgpa : ""}</div>
                 <div>{item.stage}</div>
-                <div className="flex gap-x-4">
+                <div className="flex gap-x-4 items-center">
                   {item.icons?.map((icon, itemIndex) => {
-                    return <div key={itemIndex} className="text-2xl text-white">{icon}</div>
+                    return <div key={itemIndex} className="text-2xl">{icon}</div>
                   })}
                 </div>
               </div>
